0f589a42d4c47c98a44122b9f4f6b4c2ca2d6216
Robin Savage <macbookbro@Robins-MBP.domain>
2019-01-28 17:41:24 -0600

    Next try

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,17 +1,19 @@
 (function () {
     const searchedForText = '';
     const unsplashRequest = new XMLHttpRequest();
-
-    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
     unsplashRequest.onload = addImage;
-    unsplashRequest.setRequestHeader('Authorization', 'UdacitySplash');
+    unsplashRequest.onerror = function (err) {
+        requestError = (err, 'image');
+    }
+    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 1656c584143a5c9d2205b32008e764ef8a4cc34f9a95b3c9b39464dedb492e96');
     unsplashRequest.send();
 
     function addImage(){
         let htmlContent = "";
         const data = JSON.parse(this.responseText);
         
-        if (if data && data.results && data.results[0]) {
+        if (data && data.results && data.results[0]) {
         
             const firstImage = data.results[0];
             
@@ -19,6 +21,7 @@
             <img src="${firstImage.urls.regular}" alt="${searchedForText}">
             <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
             </figure>`;
+            
         } else {
             htmlContent = '<div class="error-no-image">No images available</div>';
         }
