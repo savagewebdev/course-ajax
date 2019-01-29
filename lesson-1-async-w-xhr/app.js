a104375a146b4b479ac83415d83f9e7ed42217be
Robin Savage <macbookbro@Robins-MBP.domain>
2019-01-29 13:34:26 -0600

    Jquery-Ajax conversion

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,46 +1,76 @@
 (function () {
-        form.addEventListener('submit', function (e) {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
-    });
-    
-    const searchedForText = '';
-    const unsplashRequest = new XMLHttpRequest();
-    unsplashRequest.onload = addImage;
-    unsplashRequest.onerror = function (err) {
-        requestError = (err, 'image');
-    }
-    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 4d38c65773a6eb0bf1557e5770a8b121e2c38d56a4763a81bbd05974586dbb24');
-    unsplashRequest.send();
 
-    function addImage(){
-        let htmlContent = "";
-        const data = JSON.parse(this.responseText);
+//        Pre-jquery XHR code.
+//        const imgRequest = new XMLHttpRequest();
+//        imgRequest.onload = addImage;
+//        imgRequest.onerror = function (err) {
+//            requestError(err, 'image');
+//        };
+//        imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+//        imgRequest.setRequestHeader('Authorization', 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3');
+//        imgRequest.send();
         
-        if (data && data.results && data.results[0]) {
-        
-            const firstImage = data.results[0];
-            
+        $.ajax({ 
+            url: https: api.unsplash.com/search/photos?page=1&query=${searchedForText} 
+            headers: { 
+                Authorization: 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3' 
+            } 
+        }).done(addImage);
+
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function (err) {
+            requestError(err, 'articles');
+        };
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e6a9801dab184d89a4d77b94ff44048c`);
+        articleRequest.send();
+    });
+
+    function addImage(images) { // images parameter added.
+        let htmlContent = '';
+//        const data = JSON.parse(this.responseText); No longer needed.
+
+        if (images && images.results && images.results[0]) {
+            const firstImage = images.results[0]; // data.results[0]; beforehand.
             htmlContent = `<figure>
-            <img src="${firstImage.urls.regular}" alt="${searchedForText}">
-            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
             </figure>`;
-            
         } else {
             htmlContent = '<div class="error-no-image">No images available</div>';
         }
+
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
-    
-    function addArticles () {
-        let htmlContent
-    const articleRequest = new XMLHttpRequest();
-    articleRequest.onload = addArticles;
-    articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=<144585c9-db15-402c-92bd-e94a7ea37794
->`);
-    articleRequest.send();
-    };
 
+    function addArticles() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-${part}">Oh no! There was an error making a request for the ${part}.</p>`);
+    }
 })();
