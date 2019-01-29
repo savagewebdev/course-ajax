62296c5f5f68ef392d9aedd628368d2b9b309e77
Robin Savage <macbookbro@Robins-MBP.domain>
2019-01-28 18:05:40 -0600

    Different key attempt

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,4 +1,10 @@
 (function () {
+        form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+    });
+    
     const searchedForText = '';
     const unsplashRequest = new XMLHttpRequest();
     unsplashRequest.onload = addImage;
@@ -6,7 +12,7 @@
         requestError = (err, 'image');
     }
     unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 1656c584143a5c9d2205b32008e764ef8a4cc34f9a95b3c9b39464dedb492e96');
+    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 4d38c65773a6eb0bf1557e5770a8b121e2c38d56a4763a81bbd05974586dbb24');
     unsplashRequest.send();
 
     function addImage(){
@@ -28,16 +34,13 @@
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
     
-    function addArticles () {}
+    function addArticles () {
+        let htmlContent
     const articleRequest = new XMLHttpRequest();
     articleRequest.onload = addArticles;
     articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=<144585c9-db15-402c-92bd-e94a7ea37794
 >`);
     articleRequest.send();
-    
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
-    });
+    };
+
 })();
